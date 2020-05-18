52850e5e0e57cc04a30429ae2908f1faf338a3f6
Claudia Silva <claudia.devsp@gmail.com>
2020-05-18 18:21:06 -0300

    [CREATE] routes , forms e templates

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Inicio" subtitle="Segundo projeto do capitulo de React">
+    <div className="display-4">Bem vindo!</div>
+    <hr />
+    <p className="mb-0">
+        Exemplificando cadastro de usuário.
+    </p>
+    </Main>
--- src/components/template/Footer.jsx
+++ src/components/template/Footer.jsx
@@ -3,5 +3,7 @@ import React from 'react'
 
 export default props =>
     <footer className="footer">
-        Footer
+        <span>
+            Desenvolvido com <i className="fa fa-heart text-danger"></i> por <strong>Clau Souza</strong>
+        </span>
     </footer>
